16c35381bea4fbc5546dc243592768df28b5d1d8
joroni <jraymund.niconi@gmail.com>
2019-03-17 03:14:37 +0800

    ok

--- script.js
+++ script.js
@@ -23,40 +23,6 @@ return _this.value;
 
 
 
-/* 
-starApp.controller('StarCtrl', ['$scope', function ($scope) {
-    $scope.ratings = [{
-        current: 5,
-        max: 10
-    }, {
-        current: 3,
-        max: 5
-    }];
-}]);
-
-starApp.directive('starRating', function () {
-    return {
-        restrict: 'A',
-        template: '<ul class="rating">' +
-            '<li ng-repeat="star in stars" ng-class="star">' +
-            '\u2605' +
-            '</li>' +
-            '</ul>',
-        scope: {
-            ratingValue: '=',
-            max: '='
-        },
-        link: function (scope, elem, attrs) {
-            scope.stars = [];
-            for (var i = 0; i < scope.max; i++) {
-                scope.stars.push({
-                    filled: i < scope.ratingValue
-                });
-            }
-        }
-    }
-}); */
-
 
 app.controller('ctrl',function($scope){
   $scope.max = 5;
@@ -173,6 +139,11 @@ app.controller('peopleCtrl', function ($scope, $http) {
         console.log('vmdata', vmdata);
         $scope.myVMdata = vmdata;
 
+
+
+        $scope.messagingStat =function(){
+          alert('Message Sent.');
+        }
        
      /*    for (var i = 0; i < vmdata.length; i++) {
            var data = vmdata;
