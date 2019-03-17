d37b9216257da018e640d9b8ca04272796aa6c98
joroni <jraymund.niconi@gmail.com>
2019-03-17 22:17:59 +0800

    good updates

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
 var app = angular.module('myApp', []);
 app.controller('controller', controller);
+
 function controller() {
-    var _this = this;
-    //_this.hello = 'Hello There';
-    var vmdata = JSON.parse(localStorage.getItem('people'));
-    _this.value = _this.value;
-    _this.values = vmdata;
-    _this.change = function () {
-        //  _this.sayHello = 'Hello';
-        _this.sayHello = _this.value;
-        console.log('this', _this.values);
-return _this.value;
-    }
+  var _this = this;
+  //_this.hello = 'Hello There';
+  var vmdata = JSON.parse(localStorage.getItem('people'));
+  _this.value = _this.value;
+  _this.values = vmdata;
+  _this.change = function() {
+    //  _this.sayHello = 'Hello';
+    _this.sayHello = _this.value;
+
+    console.log('this', _this.values);
+
+
+    return _this.value;
+
+
+
+  }
 }
-app.controller('ctrl',function($scope){
+
+
+
+
+app.controller('ctrl', function($scope) {
   $scope.max = 5;
   $scope.ratingVal = 5;
   $scope.readonly = false;
-  $scope.onHover = function(val){
+  $scope.onHover = function(val) {
     $scope.hoverVal = val;
   };
-  $scope.onLeave = function(){
+  $scope.onLeave = function() {
     $scope.hoverVal = null;
   }
-  $scope.onChange = function(val){
+  $scope.onChange = function(val) {
     $scope.ratingVal = val;
   }
+
 });
-app.directive('star', function () {
+app.directive('star', function() {
   return {
     template: '<ul class="rating" ng-mouseleave="leave()">' +
-        '<li ng-repeat="star in stars" ng-class="star" ng-click="click($index + 1)" ng-mouseover="over($index + 1)">' +
-        '<i class="fa fa-star fa-1x"></i>' +
-        '</li>' +
-        '</ul>',
+      '<li ng-repeat="star in stars" ng-class="star" ng-click="click($index + 1)" ng-mouseover="over($index + 1)">' +
+      '<i class="fa fa-star fa-1x"></i>' +
+      '</li>' +
+      '</ul>',
     scope: {
       ratingValue: '=',
       max: '=',
@@ -41,25 +53,25 @@ app.directive('star', function () {
       onHover: '=',
       onLeave: '='
     },
-    controller: function($scope){
+    controller: function($scope) {
       $scope.ratingValue = $scope.ratingValue || 0;
       $scope.max = $scope.max || 5;
-      $scope.click = function(val){
+      $scope.click = function(val) {
         if ($scope.readonly && $scope.readonly === 'true') {
           return;
         }
         $scope.ratingValue = val;
       };
-      $scope.over = function(val){
+      $scope.over = function(val) {
         $scope.onHover(val);
       };
-      $scope.leave = function(){
+      $scope.leave = function() {
         $scope.onLeave();
       }
     },
-    link: function (scope, elem, attrs) {
+    link: function(scope, elem, attrs) {
       elem.css("text-align", "center");
-      var updateStars = function () {
+      var updateStars = function() {
         scope.stars = [];
         for (var i = 0; i < scope.max; i++) {
           scope.stars.push({
@@ -68,12 +80,13 @@ app.directive('star', function () {
         }
       };
       updateStars();
-      scope.$watch('ratingValue', function (oldVal, newVal) {
+
+      scope.$watch('ratingValue', function(oldVal, newVal) {
         if (newVal) {
           updateStars();
         }
       });
-      scope.$watch('max', function (oldVal, newVal) {
+      scope.$watch('max', function(oldVal, newVal) {
         if (newVal) {
           updateStars();
         }
@@ -93,42 +106,116 @@ app.controller('ngchangeCtrl', function ($scope) {
         "userid": 3,
         "name": "Praveen"
     }];
+
     var un =  $scope.arrlist;
     $scope.getdetails = function () {
         if ($scope.userselected.userid == $scope.userselected.userid )
             $scope.result = true;
+
         else
             $scope.result = false;
     }
 }); */
+
+
 app.controller('ExampleController', ['$scope', function($scope) {
-    $scope.name = 'Whirled';
-  }]);
-app.controller('peopleCtrl', function ($scope, $http) {
-    $http.get("people.json").then(function (response) {
-        $scope.myData = response.data.People;
-        $scope.boom = function (val) {
-            console.log(val);
-        };
-        console.log('myData', $scope.myData);
-        localStorage.setItem('people', JSON.stringify($scope.myData));
-        var vmdata = JSON.parse(localStorage.getItem('people'));
-        console.log('vmdata', vmdata);
-        $scope.myVMdata = vmdata;
-        $scope.messagingStat =function(){
-          alert('Message Sent.');
+  $scope.name = 'Whirled';
+}]);
+
+app.controller('Test',  function($scope) {
+    $scope.persons = [{ type: 1, name: 'Caio' }, { type: 2, name: 'Ary' }, { type: 1, name: 'Camila' }, { type: 3, name: 'Daniel' }];
+    $scope.myFunction = function (val) {
+
+        return (val.type != 2);
+    };
+});
+
+/* 
+app.controller('peopleCtrl2',  function($scope) {
+
+
+    var vmdata = JSON.parse(localStorage.getItem('people'));
+    console.log('vmdata', vmdata);
+    $scope.myVMdata = vmdata;
+    $scope.persons = vmdata;
+    $scope.myFunction = function (val) {
+
+        return (val.type != 2);
+    };
+});
+ */
+
+
+app.controller('peopleCtrl', function($scope, $http) {
+  $http.get("user.json").then(function(response) {
+    $scope.mockUserData = response.data.User;
+    $scope.username = function(val) {
+      console.log(val);
+    };
+  })
+  $http.get("people.json").then(function(response) {
+    $scope.myData = response.data.People;
+    /* $scope.boom = function(val) {
+      console.log(val);
+    };
+ */
+
+    //console.log('mockUserData', $scope.mockUserData);
+    localStorage.setItem('user', JSON.stringify($scope.mockUserData));
+
+
+    var vmuserdata = JSON.parse(localStorage.getItem('user'));
+    console.log('vmuserdata', vmuserdata);
+    $scope.userVMdata = vmuserdata;
+
+
+    //console.log('myData', $scope.myData);
+    localStorage.setItem('people', JSON.stringify($scope.myData));
+
+
+    var vmdata = JSON.parse(localStorage.getItem('people'));
+    console.log('vmdata', vmdata);
+    $scope.myVMdata = vmdata;
+
+
+
+    $scope.messagingStat = function() {
+      alert('Message Sent.');
+    }
+
+ 
+
+    $scope.getName = function(name) {
+      console.log(name);
+    }
+
+ 
+    $scope.theFilter = '';
+    $scope.getID = function(id) {
+      $scope.searchText=  '';
+      console.log('id: ',  id);
+    
+      $scope.theFilter = id;
+      console.log( 'Filter: ',$scope.theFilter);
+
+
+    }
+
+
+
+    $scope.myfield = '';
+    $scope.checkContent = function(){
+        if($scope.myfield)//Check Empty Field
+        {
+            $scope.msg = "pls enter something";
+        }else{
+            $scope.msg = "Something Entered";
         }
-     /*    for (var i = 0; i < vmdata.length; i++) {
-           var data = vmdata;
-         //  console.log(index); // 0, 1, 2, 3
-            console.log(data); // A, B, C, D
-          } */
-          $scope.getID= function(id){
-            // $scope.theFilter = {};
-            console.log(id);
-           // console.log($scope.theFilter);
-            $scope.theFilter =  $scope.myVMdata;
-           // console.log($scope.theFilter.id);
-          }
-    });
+    } 
+
+
+
+
+  });
+
 });
